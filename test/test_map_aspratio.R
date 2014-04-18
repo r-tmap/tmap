978251c3c1093ee91781124c3dc031bd08e26e0a
Martijn Tennekes <mtennekes@gmail.com>
2014-04-18 15:29:25 +0200

    added asp ratio file

---
+++ test/test_map_aspratio.R
@@ -0,0 +1,16 @@
+data(Europe)
+
+geo(Europe, choro.fill="gdp_cap_est", borders="gray50", theme="Europe", style="kmeans", title="Estimated GDP per capita")
+
+geo_shape(Europe) +
+	geo_choropleth("gdp_cap_est", style="kmeans") +
+	geo_borders("gray50") +
+	geo_theme_Europe("Estimated GDP per capita")
+
+geo_shape(Europe) +
+	geo_choropleth("gdp_cap_est", style="kmeans") +
+	geo_borders("gray50") + #geo_theme()
+	geo_theme_Europe("Estimated GDP per capita", draw.frame=FALSE, crop=FALSE,
+					 margins = rep(.1, 4), frame.margins=c(0, 0, 0, 0))
+
+geo_theme()
