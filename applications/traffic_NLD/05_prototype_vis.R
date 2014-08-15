abb943abb9a0017a058ee2e1e574f51102c60374
Martijn Tennekes <mtennekes@gmail.com>
2014-08-15 18:45:06 +0200

    updated prototype1

--- applications/traffic_NLD/05_prototype_vis.R
+++ applications/traffic_NLD/05_prototype_vis.R
@@ -20,11 +20,18 @@ str(corop@data)
 
 cols <- treepalette(corop@data, index = c("NUTS1", "NUTS2", "NUTS3"), palette.HCL.options = list(hue_fraction=.8, hue_perm=TRUE, hue_rev=TRUE, hue_start=90))
 
-corop <- append_data(corop, cols[!is.na(cols$NUTS3),c("NUTS3", "HCL.color")], key.data = "NUTS3", key.shp = "NUTS3")
+corop <- append_data(corop, cols[!is.na(cols$NUTS3),c("NUTS1", "NUTS2", "NUTS3", "HCL.color")], key.data = "NUTS3", key.shp = "NUTS3")
 names(corop) <- gsub(".", "_", names(corop), fixed = TRUE)
 
 prov <- unionSpatialPolygons(corop, corop$NUTS2)
-prov <- append_data(prov, data=data.frame(ID=get_IDs(prov)))
+prov <- append_data(prov, data=data.frame(NUTS2=get_IDs(prov)))
+prov$HCL_color <- corop$HCL_color[match(prov$NUTS2, corop$NUTS2)]
+
+land <- unionSpatialPolygons(corop, corop$NUTS1)
+land <- append_data(land, data=data.frame(NUTS1=get_IDs(land)))
+land$HCL_color <- corop$HCL_color[match(land$NUTS1, corop$NUTS1)]
+
+
 
 tm_shape(corop) +
 	tm_fill("HCL_color") + 
@@ -35,20 +42,26 @@ tm_shape(prov) +
 ## create geojson objects
 corop <- set_projection(corop, "longlat")
 prov <- set_projection(prov, "longlat")
+land <- set_projection(land, "longlat")
 drw_nuts <- set_projection(drw_nuts, "longlat")
 
-writeOGR(corop, '../applications/traffic_NLD/prototype_vis/corop.geojson','dataMap', driver='GeoJSON')
-writeOGR(prov, '../applications/traffic_NLD/prototype_vis/prov.geojson','dataMap', driver='GeoJSON')
+writeOGR(corop, '../applications/traffic_NLD/prototype_vis/nuts3.geojson','dataMap', driver='GeoJSON')
+writeOGR(prov, '../applications/traffic_NLD/prototype_vis/nuts2.geojson','dataMap', driver='GeoJSON')
+writeOGR(land, '../applications/traffic_NLD/prototype_vis/nuts1.geojson','dataMap', driver='GeoJSON')
 writeOGR(drw_nuts, '../applications/traffic_NLD/prototype_vis/drw_nuts.geojson','dataMap', driver='GeoJSON')
 
 ## rewrite as js files
-input<-readLines("../applications/traffic_NLD/prototype_vis/corop.geojson") 
-input[1] <- paste("var corop =", input[1])
-writeLines(input, "../applications/traffic_NLD/prototype_vis/corop.js") 
+input<-readLines("../applications/traffic_NLD/prototype_vis/nuts3.geojson") 
+input[1] <- paste("var nuts3 =", input[1])
+writeLines(input, "../applications/traffic_NLD/prototype_vis/nuts3.js") 
 
-input<-readLines("../applications/traffic_NLD/prototype_vis/prov.geojson") 
-input[1] <- paste("var prov =", input[1])
-writeLines(input, "../applications/traffic_NLD/prototype_vis/prov.js") 
+input<-readLines("../applications/traffic_NLD/prototype_vis/nuts2.geojson") 
+input[1] <- paste("var nuts2 =", input[1])
+writeLines(input, "../applications/traffic_NLD/prototype_vis/nuts2.js") 
+
+input<-readLines("../applications/traffic_NLD/prototype_vis/nuts1.geojson") 
+input[1] <- paste("var nuts1 =", input[1])
+writeLines(input, "../applications/traffic_NLD/prototype_vis/nuts1.js") 
 
 input<-readLines("../applications/traffic_NLD/prototype_vis/drw_nuts.geojson") 
 input[1] <- paste("var drw =", input[1])
