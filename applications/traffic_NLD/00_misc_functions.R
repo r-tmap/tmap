7d21308a0c5bdf32d74a042a83ad488f19de94c9
Martijn Tennekes <mtennekes@gmail.com>
2014-10-01 20:29:19 +0200

    ndw viz updated

--- applications/traffic_NLD/00_misc_functions.R
+++ applications/traffic_NLD/00_misc_functions.R
@@ -16,6 +16,14 @@ get_direction_angle <- function(co) {
 	a
 }
 
+get_cycle_direction <- function(co) {
+	p1 <- co[1,]
+	p2 <- co[floor(nrow(co)/2),]
+	a1 <- get_direction_angle(co[c(1, floor(nrow(co)*.45)), ])
+	a2 <- get_direction_angle(co[c(1, floor(nrow(co)*.55)), ])
+	if ((a1 > a2) && (a1-a2 < 180)) return("CW") else return("CCW")
+}
+
 # get angle in degrees that correspond to the direction NOZW
 get_NOZW_angle <- function(direction) {
 	switch(direction, 
@@ -26,7 +34,8 @@ get_NOZW_angle <- function(direction) {
 		   Z=270,
 		   ZW=225,
 		   W=180,
-		   NW=135)
+		   NW=135,
+		   NULL)
 }
 
 # return the absolute difference of two angles in degrees
@@ -44,42 +53,61 @@ set_directions <- function(lines, main_direction=TRUE, directions=directions) {
 	rns <- lines$ID
 	
 	co2 <- mapply(function(p, rn) {
-		a_rw <- get_NOZW_angle(directions$directionR[directions$roadname==rn])
-		if (!main_direction) a_rw <- a_rw + 180
+		cat("rijksweg ", rn, "\n")
 		
-		## set direction per polyline segment
-		p2 <- lapply(p, function(l) {
-			a1 <- get_direction_angle(l)
-			adiff <- angle_diff(a1, a_rw)
-			if (adiff > 90) l <- l[nrow(l):1,]
-			l
-		})
+		#if (rn=="A10") browser()
 		
-		## set order of polyline segments
-		if (length(p2)>1) {
-			line_centers <- lapply(p2, function(l) {
-				colMeans(l)
+		dir_string <- directions$directionR[directions$roadname==rn]
+		a_rw <- get_NOZW_angle(dir_string)
+		
+		if (is.null(a_rw)) {
+			if (length(p)!=1) stop("rondweg heeft meerdere segmenten")
+			p1 <- p[[1]]
+			dir1 <- get_cycle_direction(p1)
+			if (dir1 != dir_string) p1 <- p1[nrow(p1):1,]
+			p2 <- list(p1)	
+			
+		} else {
+			if (!main_direction) a_rw <- a_rw + 180
+			
+			
+			## set direction per polyline segment
+			p2 <- lapply(p, function(l) {
+				a1 <- get_direction_angle(l)
+				adiff <- angle_diff(a1, a_rw)
+				if (adiff > 90) l <- l[nrow(l):1,]
+				l
 			})
 			
-			line_centers <- do.call("rbind", line_centers)
-			line_centers <- SpatialPointsDataFrame(line_centers, data.frame(ID=1:nrow(line_centers)))
-			line_centers_co <- coordinates(line_centers)
-			
-			
-			line_fit <- fit_polylines(line_centers, sep.dist = 1e6, verbose = FALSE)
-			line_fit_co <- coordinates(line_fit)[[1]][[1]]
-			line_fit_a <- get_direction_angle(line_fit_co)
-			adiff <- angle_diff(line_fit_a, a_rw)
-			if (adiff > 90) line_fit_co <- line_fit_co[nrow(line_fit_co):1, ]
-			
-			id_line_fit <- paste(round(line_fit_co[,1], 2), round(line_fit_co[,2], 2), sep="_")
-			id_line_centers <- paste(round(line_centers_co[,1], 2), round(line_centers_co[,2], 2), sep="_")
-			
-			stopifnot(setequal(id_line_fit, id_line_centers))
-			
-			p2 <- p2[match(id_line_centers, id_line_fit)]
+			## set order of polyline segments
+			if (length(p2)>1) {
+				line_centers <- lapply(p2, function(l) {
+					colMeans(l)
+				})
+				
+				line_centers <- do.call("rbind", line_centers)
+				line_centers <- SpatialPointsDataFrame(line_centers, data.frame(ID=1:nrow(line_centers)))
+				line_centers_co <- coordinates(line_centers)
+				
+				
+				line_fit <- fit_polylines(line_centers, sep.dist = 1e6, verbose = FALSE)
+				line_fit_co <- coordinates(line_fit)[[1]][[1]]
+				line_fit_a <- get_direction_angle(line_fit_co)
+				adiff <- angle_diff(line_fit_a, a_rw)
+				if (adiff > 90) line_fit_co <- line_fit_co[nrow(line_fit_co):1, ]
+				
+				id_line_fit <- paste(round(line_fit_co[,1], 2), round(line_fit_co[,2], 2), sep="_")
+				id_line_centers <- paste(round(line_centers_co[,1], 2), round(line_centers_co[,2], 2), sep="_")
+				
+				stopifnot(setequal(id_line_fit, id_line_centers))
+				
+				p2 <- p2[match(id_line_centers, id_line_fit)]
+				
+			}
 			
 		}
+			
+		
 		sapply(p2, Line)
 	}, co, rns, SIMPLIFY=FALSE)
 	
--- applications/traffic_NLD/02_doorlopende_rijkswegen.R
+++ applications/traffic_NLD/02_doorlopende_rijkswegen.R
@@ -6,7 +6,11 @@ library(rgeos)
 ### load preprocessed data
 load("../applications/traffic_NLD//throughput/loops.rda")
 load("../applications/traffic_NLD//throughput/rijkswegen.rda")
+load("../applications/traffic_NLD//throughput/corop.rda")
 source("../applications/traffic_NLD//00_misc_functions.R")
+
+
+directions <- read.csv2("../applications/traffic_NLD/input/richtingen_rijkswegen.txt", stringsAsFactors=FALSE)
 
 
 ###### TEMP SELECTION
@@ -19,8 +23,14 @@ rwR <- rwR[rwR$roadname %in% road, ]
 loops <- loops[loops$roadname %in% road,]
 
 
+
+
+
+
+
 ## CREATE CONTINUOUS POLYLINES
 
+# todo: finetune foor afgebroken lijnen, zoals A9/A2
 system.time({
 	drwL <- fit_polylines(rwL[rwL$BAANSUBSRT=="HR", ], id="roadname")
 })
@@ -29,6 +39,30 @@ system.time({
 	drwR <- fit_polylines(rwR[rwR$BAANSUBSRT=="HR", ], id="roadname")
 })
 
+## REVERT IF NECASSARY
+drwL <- set_directions(drwL, main_direction = FALSE, directions = directions)
+drwR <- set_directions(drwR, main_direction = TRUE, directions = directions)
+
+
+save(drwL, drwR, file="../applications/traffic_NLD/throughput/doorlopende_rijkswegen.rda")
+
+
+
+pdf("../applications/traffic_NLD/plots/drw.pdf")
+tm_shape(corop) +
+	tm_fill() +
+tm_shape(drwR) +
+	tm_lines(col="red") +
+tm_shape(drwL) +
+	tm_lines(col="blue") +
+tm_layout(scale=.1)
+dev.off()
+
+
+
+############ FOR ROAD SEGMENTS ##############################
+
+
 
 ## SPLIT INTO EQUAL PARTS
 
@@ -36,64 +70,55 @@ dist <- 10
 drwL2 <- split_lines_equal(drwL, dist=dist)
 drwR2 <- split_lines_equal(drwR, dist=dist)
 
-## REVERT IF NECASSARY
 
-directions <- read.csv2("../applications/traffic_NLD/input/richtingen_rijkswegen.txt", stringsAsFactors=FALSE)
-#roads <- read.csv2("../applications/traffic_NLD/throughput/roads.csv", stringsAsFactors=FALSE)
-
-
-drwL3 <- set_directions(drwL2, main_direction = FALSE, directions = directions)
-drwR3 <- set_directions(drwR2, main_direction = TRUE, directions = directions)
+listL <- create_meter_list(drwL2, dist=10, direction="L")
+listR <- create_meter_list(drwR2, dist=10, direction="R")
 
 
-drwL4 <- create_meter_list(drwL3, dist=10, direction="L")
-drwR4 <- create_meter_list(drwR3, dist=10, direction="R")
-
-
-loopsL <- search_points(loops, drwL3)
+loopsL <- search_points(loops, drwL2)
 loopsL <- lapply(loopsL, function(l) l[l$d<20, ]) ## todo: use loop meta data
 
-loopsR <- search_points(loops, drwR3)
+loopsR <- search_points(loops, drwR2)
 loopsR <- lapply(loopsR, function(l) l[l$d<20, ]) ## todo: use loop meta data
 
-drwL4 <- write_point_info(loopsL, drwL4, "LUS")
-drwR4 <- write_point_info(loopsR, drwR4, "LUS")
+listL <- write_point_info(loopsL, listL, "LUS")
+listR <- write_point_info(loopsR, listR, "LUS")
 
 
 
-afrL <- search_POB(rwL[rwL$BAANSUBSRT=="AFR", ], drwL3) 
-afrR <- search_POB(rwR[rwR$BAANSUBSRT=="AFR", ], drwR3) 
+afrL <- search_POB(rwL[rwL$BAANSUBSRT=="AFR", ], drwL2) 
+afrR <- search_POB(rwR[rwR$BAANSUBSRT=="AFR", ], drwR2) 
 
 
 
-drwL4 <- write_point_info(afrL, drwL4, "AFRIT")
-drwR4 <- write_point_info(afrR, drwR4, "AFRIT")
+listL <- write_point_info(afrL, listL, "AFRIT")
+listR <- write_point_info(afrR, listR, "AFRIT")
 
 
 
-oprL <- search_POB(rwL[rwL$BAANSUBSRT=="OPR", ], drwL3) 
-oprR <- search_POB(rwR[rwR$BAANSUBSRT=="OPR", ], drwR3) 
+oprL <- search_POB(rwL[rwL$BAANSUBSRT=="OPR", ], drwL2) 
+oprR <- search_POB(rwR[rwR$BAANSUBSRT=="OPR", ], drwR2) 
 
-drwL4 <- write_point_info(oprL, drwL4, "OPRIT")
-drwR4 <- write_point_info(oprR, drwR4, "OPRIT")
+listL <- write_point_info(oprL, listL, "OPRIT")
+listR <- write_point_info(oprR, listR, "OPRIT")
 
-write_info(drwL4, path="../applications/traffic_NLD/output")
-write_info(drwR4, path="../applications/traffic_NLD/output")
-
-pdf("../applications/traffic_NLD/plots/rijkswegen_met_info.pdf", width = 10, height=10) 
-plot_drw(drwL3, drwL4, scale=.1)
-dev.off()
-
-drw <- drwL3
-info <- drwL4
-
-plot_per_rw(drwL3, drwL4, path="../applications/traffic_NLD/plots")
-plot_per_rw(drwR3, drwR4, path="../applications/traffic_NLD/plots")
-
-plot_google(drwL3, drwL4, "A79")
-plot_google(drwR3, drwR4, "A79")
-
-
-info <- drwL4
-rn <- "A79"
+write_info(listL, path="../applications/traffic_NLD/output")
+write_info(listR, path="../applications/traffic_NLD/output")
+# 
+# pdf("../applications/traffic_NLD/plots/rijkswegen_met_info.pdf", width = 10, height=10) 
+# plot_drw(drwL3, drwL4, scale=.1)
+# dev.off()
+# 
+# drw <- drwL3
+# info <- drwL4
+# 
+# plot_per_rw(drwL3, drwL4, path="../applications/traffic_NLD/plots")
+# plot_per_rw(drwR3, drwR4, path="../applications/traffic_NLD/plots")
+# 
+# plot_google(drwL3, drwL4, "A79")
+# plot_google(drwR3, drwR4, "A79")
+# 
+# 
+# info <- drwL4
+# rn <- "A79"
 
