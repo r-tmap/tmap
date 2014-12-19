4bf58c9368379b068bd7b8f0575b7cc7908401f4
Martijn Tennekes <mtennekes@gmail.com>
2014-12-19 17:59:41 +0100

    added intersection_shapes

--- pkg/R/convert_shape_data.R
+++ pkg/R/convert_shape_data.R
@@ -1,18 +1,39 @@
 #' Convert shape data
 #' 
-#' Convert data from one polygon shape to another. It uses an intersection matrix, which stores the intersection ratios of the two shape objects per polygon
+#' Convert numeric data from one polygon shape to another. It uses an intersection matrix, which stores the intersection ratios of the two shape objects per polygon (see \code{\link{intersection_shapes}).
 #' 
-#' @param shp.from the shape object to be converted. It sould contain data.
+#' @param shp.from the shape object to be converted. It should contain data.
 #' @param shp.to the shape object to be converted into.
-#' @param variables.from names of the variable of \code{shp.from} to be converted. If missing, all numeric variables are taken.
+#' @param variables.from names of the numeric variables of \code{shp.from} to be converted. If missing, all numeric variables are taken.
 #' @param variables.to variable names to be used. Should be the same number of variable names as \code{variables.from}
 #' @import rgeos
 #' @return shape object \code{shp.to} with converted data from \code{shp.from}
 #' @export
 convert_shape_data <- function(shp.from, shp.to, variables.from=NULL, variables.to=NULL) {
+	x <- intersection_shapes(shp.from, shp.to)
+
+	data.from <- shp.from@data
+	if (missing(variables.from)) variables.from <- names(data.from)[sapply(data.from, is.numeric)]
+	if (missing(variables.to)) variables.to <- variables.from
+	y <- x %*% as.matrix(data.from[, variables.from])
+	dimnames(y)[[2]] <- variables.to
+	shp.to <- append_data(shp.to, y, fixed.order=TRUE)
+	shp.to
+}
+
+#' Calculate intersection matrix between two polygon shapes
+#' 
+#' The intersection matrix contains all intersection ratios from the polygons of the first shape object to the polygons of the second shape object
+#' 
+#' @param shp.from the shape object to be converted. It should contain data.
+#' @param shp.to the shape object to be converted into.
+#' @import rgeos
+#' @return intersection matrix
+#' @export
+intersection_shapes <- function(shp.from, shp.to) {
 	polys.to <- shp.to@polygons
 	polys.from <- shp.from@polygons
-	x <- sapply(polys.from, function(p.from) {
+	sapply(polys.from, function(p.from) {
 		sapply(polys.to, function(p.to) {
 			gInt <- gIntersection(SpatialPolygons(list(p.to)), SpatialPolygons(list(p.from)))
 			if (is.null(gInt)) return(0) else {
@@ -25,11 +46,4 @@ convert_shape_data <- function(shp.from, shp.to, variables.from=NULL, variables.
 			return(0)
 		})
 	})
-	data.from <- shp.from@data
-	if (missing(variables.from)) variables.from <- names(data.from)[sapply(data.from, is.numeric)]
-	if (missing(variables.to)) variables.to <- variables.from
-	y <- x %*% as.matrix(data.from[, variables.from])
-	dimnames(y)[[2]] <- variables.to
-	shp.to <- append_data(shp.to, y, fixed.order=TRUE)
-	shp.to
 }
