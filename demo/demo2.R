c2e1a82b163139da6038183ef35860af0d4517af
Martijn Tennekes <mtennekes@gmail.com>
2015-02-10 09:58:41 +0100

    ...

--- demo/demo2.R
+++ demo/demo2.R
@@ -3,34 +3,56 @@
 # https://www.census.gov/geo/maps-data/data/cbf/cbf_counties.html
 
 devtools::load_all(".")
+# library(tmap)
+library(raster)
+library(rgeos)
+library(ggplot2)
+library(maps)
+library(stringdist)
+library(plyr)
+library(scales)
+library(RColorBrewer)
 
+## read and crop shape 
 zipfile = "../demo/shape/gz_2010_us_050_00_500k.zip"
 zipdir <- tempdir()
 unzip(zipfile, exdir=zipdir)
 
-
-# library(tmap)
-
-library(raster)
-library(rgeos)
-
 shp <-  read_shape(file = paste0(zipdir, "/gz_2010_us_050_00_500k.shp"))
 shp$fips <- paste0(shp$STATE, shp$COUNTY)
 
+data(state.fips)
+shp$state_name <- as.character(state.fips$polyname[match(as.integer(as.character(shp$STATE)), state.fips$fips)])
+shp$state_name <- sub(":.*", "", shp$state_name)
+shp$mpname <- paste(tolower(shp$state_name), tolower(shp$NAME), sep=",")
 
 shp <- crop_shape(shp, matrix(c(-130, -60, 25, 50), nrow = 2, byrow = TRUE))
 
-# us_rect <- as(extent(-130, -60, 25, 50), "SpatialPolygons")
-# us_rect <- set_projection(us_rect, current.projection = get_projection(shp))
-# 
-# shp <- append_data(as.data.frame(coordinates(shp)), shp, fixed.order = TRUE)
-# shp <- shp[shp$V1> -130 & shp$V1 < -60 & shp$V2 > 25 & shp$V2 < 50, ]
 
-shp1 <- shp[1,]
+## process data
+unemp <- read.csv('../demo/data/US_unemployment09.csv',header=FALSE, stringsAsFactors=FALSE,
+				  col.names=c("blsid", "stfips", "cofips", "name", "year", "pop1", "pop2", "unempraw", "unemppct"))
 
-system.time({
-	print(qplot(mpg, wt, data=mtcars))
-})
+unemp$mpname <- tolower(paste(state.name[match(
+	sub("^.*([A-Z][A-Z])$","\\1",unemp$name,fixed=FALSE),state.abb)],
+	sub("^(.*) (County|[Cc]ity|Parish), ..$","\\1", unemp$name),sep=","))
+unemp$mpname <- sub(":.*", "", unemp$mpname)
+unemp$mpname <- sub(" county/(city|town),.*", "", unemp$mpname)
+#unemp$mpname <- sub(" county/town,.*", "", unemp$mpname)
+
+
+## match data to shp
+(x <- setdiff(shp$mpname, unemp$mpname))
+res <- amatch(x, unemp$mpname, method = "osa", weight = c(d = .001, i = 1, s = 1, t = .001), maxDist=10)
+
+shp$mpname[match(x, shp$mpname)] <- unemp$mpname[res]
+
+## append data to shp
+shp <- append_data(unemp, shp, key.data = "mpname", key.shp = "mpname", ignore.duplicates = TRUE)
+
+qtm(shp, fill="unemppct", style="kmeans", palette="PuRd")
+
+
 
 system.time({
 	print(qtm(shp))
@@ -41,13 +63,24 @@ system.time({
 })
 
 
+## test ggplot2
+shp.points <- fortify(shp, region="fips")
+shp.points$fips <- shp.points$id
+shp.df <- join(shp.points, shp@data, by="fips")
 
+shp.df$unemppct_d <- cut(shp.df$unemppct, breaks = c(seq(0, 10, by = 2), 35))
+
+system.time({
+	print(ggplot(shp.df, aes(long, lat, group = group)) +
+		geom_polygon(aes(fill = unemppct_d), colour = alpha("white", 1/2), size = 0.2) +
+		#geom_polygon(data = state_df, colour = "white", fill = NA) +
+		scale_fill_brewer(palette = "PuRd"))
+})
 
 
 system.time({
-	print(tm_shape(shp1) + tm_fill())
+	print(tm_shape(shp) + tm_fill("unemppct", style="kmeans", palette="PuRd") + tm_borders("white"))
 })
-
 
 
 system.time({
@@ -57,59 +90,4 @@ system.time({
 system.time({
 	plot(shp)
 })
-
-
-library(maps)
-unemp <- read.csv('../demo/data/US_unemployment09.csv',header=FALSE, stringsAsFactors=FALSE,
-				  col.names=c("blsid", "stfips", "cofips", "name", "year", "pop1", "pop2", "unempraw", "unemppct"))
-
-unemp$mpname <- tolower(paste(state.name[match(
-	sub("^.*([A-Z][A-Z])$","\\1",unemp$name,fixed=FALSE),state.abb)],
-	sub("^(.*) (County|[Cc]ity|Parish), ..$","\\1", unemp$name),sep=","))
-unemp$mpname <- sub(":.*", "", unemp$mpname)
-unemp$mpname <- sub(" county/city,.*", "", unemp$mpname)
-unemp$mpname <- sub(" county/town,.*", "", unemp$mpname)
-
-
-
-
-
-mp <- map("county", plot=FALSE,namesonly=TRUE)
-
-data(countyMapEnv)
-data(state.fips)
-
-
-
-
-data(county.fips)
-str(county.fips)
-
-
-setdiff(mp, unemp$mpname)
-setdiff(unemp$mpname, mp)
-
-shp$state_name <- as.character(state.fips$polyname[match(as.integer(as.character(shp$STATE)), state.fips$fips)])
-shp$state_name <- sub(":.*", "", shp$state_name)
-
-shp$mpname <- paste(tolower(shp$state_name), tolower(shp$NAME), sep=",")
-
-setdiff(shp$mpname, county.fips$polyname)
-setdiff(county.fips$polyname, shp$mpname)
-
-x <- setdiff(shp$mpname, unemp$mpname)
-
-require(stringdist)
-res <- amatch(x, unemp$mpname, method = "osa", weight = c(d = .001, i = 1, s = 1, t = .001), maxDist=10)
-
-
-shp$mpname[match(x, shp$mpname)] <- unemp$mpname[res]
-
-setdiff(shp$mpname, unemp$mpname)
-
-length(unique(shp$mpname))
-
-shp <- append_data(unemp, shp, key.data = "mpname", key.shp = "mpname", ignore.duplicates = TRUE)
-
-qtm(shp, fill="unemppct", style="kmeans", palette="PuRd")
 
