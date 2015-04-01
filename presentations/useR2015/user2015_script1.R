2c6ac2742f97e5a3c743af701c294360645d6bc5
Martijn Tennekes <mtennekes@gmail.com>
2015-04-01 21:22:03 +0200

    ...

--- presentations/useR2015/user2015_script1.R
+++ presentations/useR2015/user2015_script1.R
@@ -18,6 +18,75 @@ ggplot(NLD_data_sel, aes(y=population, x=name)) + geom_bar(stat="identity") + co
 
 qtm(NLD_muni, fill="gold", borders="purple", title="Netherlands")
 
-p <- par(mar=c(0,0,0,0))
-plot(NLD_muni, col = "gold", border="purple")
-title("Netherlands", line = -2)
+# p <- par(mar=c(0,0,0,0))
+# plot(NLD_muni, col = "gold", border="purple")
+# title("Netherlands", line = -2)
+
+qtm(NLD_muni, fill="gold", borders="purple", title="Netherlands")
+qtm(NLD_muni, fill="population", convert2density=TRUE, title="Population per km2")
+qtm(NLD_muni, fill="population", convert2density=TRUE, fill.style="kmeans", title="Population per km2")
+
+png("../presentations/useR2015/NLD1.png", width=475, height=525, res=96)
+	qtm(NLD_muni) +
+	tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+png("../presentations/useR2015/NLD2.png", width=475, height=525, res=96)
+qtm(NLD_muni, fill="gold", borders="purple", title="Netherlands") +
+	tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+png("../presentations/useR2015/NLD3.png", width=475, height=525, res=96)
+qtm(NLD_muni, fill="population", convert2density=TRUE, title="Population per km2") +
+	tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+png("../presentations/useR2015/NLD4.png", width=475, height=525, res=96)
+qtm(NLD_muni, fill="population", convert2density=TRUE, fill.style="kmeans", title="Population per km2") +
+	tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+
+png("../presentations/useR2015/NLD5.png", width=475, height=525, res=96)
+tm_shape(NLD_muni) +
+	tm_borders() +
+	tm_fill("population", convert2density = TRUE, style= "kmeans") +
+	tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+png("../presentations/useR2015/NLD6.png", width=475, height=525, res=96)
+tm_shape(NLD_muni) +
+	tm_borders(alpha = .5) +
+	tm_fill("population", convert2density = TRUE, style= "kmeans") +
+tm_shape(NLD_prov) +
+	tm_borders(lwd=3) +
+	tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+png("../presentations/useR2015/NLD7.png", width=475, height=525, res=96)
+tm_shape(NLD_muni) +
+	tm_borders(alpha = .5) +
+	tm_fill("population", convert2density = TRUE, style= "kmeans") +
+tm_shape(NLD_prov) +
+	tm_borders(lwd=3) +
+	tm_text("name", shadow=TRUE, bg.color="white", bg.alpha=.25) +
+	tm_layout(outer.margins=0, asp=0, scale=.9) 
+dev.off()
+
+
+
+png("../presentations/useR2015/NLD8.png", width=525, height=525, res=96)
+tm_shape(NLD_muni) +
+	tm_borders(alpha = .5) +
+	tm_fill("population", convert2density = TRUE, style= "kmeans") +
+tm_shape(NLD_prov) +
+	tm_borders(lwd=3) +
+	tm_text("name", shadow=TRUE, bg.color="white", bg.alpha=.25) +
+tm_layout("Population per km2", draw.frame=FALSE, bg.color="white", inner.margins=c(.02, .1, .02, .02)) +
+tm_layout(outer.margins=0, asp=0, scale=.9)
+dev.off()
+
+
+data(Europe)
+
+
