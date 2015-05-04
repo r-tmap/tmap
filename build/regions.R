3b7278c40290dd210bc4d7d24ab4ffb5567b8eab
Martijn Tennekes <mtennekes@gmail.com>
2015-05-04 11:33:08 +0200

    removed junk

--- build/regions.R
+++
@@ -1,3 +0,0 @@
-regions <- read.csv2("regions.csv", header=FALSE)
-names(regions) <- c("region", "code")
-save(regions, file="../pkg/R/sysdata.rda")
--- test/test_legend3.R
+++
@@ -1,8 +0,0 @@
-data(World)
-
-tm_shape(World[World$continent=="Africa",]) +
-	tm_fill("pop_est")
-
-tm_shape(World) +
-	tm_fill("pop_est") +
-	tm_layout_World()
