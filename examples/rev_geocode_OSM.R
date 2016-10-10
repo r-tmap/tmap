b2c0349c11437eeebd3a155102f83c5ad9ec58a1
mtennekes <mtennekes@gmail.com>
2016-10-10 21:05:47 +0200

    updated docs

---
+++ examples/rev_geocode_OSM.R
@@ -0,0 +1,13 @@
+\dontrun{
+data(metro)
+
+five_cities <- metro[sample(length(metro), 5), ]
+
+addresses <- rev_geocode_OSM(five_cities, zoom = 6)
+
+five_cities <- append_data(five_cities, addresses, fixed.order = TRUE)
+
+tmap_mode("view")
+tm_shape(five_cities) +
+	tm_markers(text="city")
+}
