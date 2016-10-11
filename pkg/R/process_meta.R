9f5de52fe4d5eedab4569e0bc8e02ff276f91af2
mtennekes <mtennekes@gmail.com>
2016-10-11 18:26:12 +0200

    fixed #76

--- pkg/R/process_meta.R
+++ pkg/R/process_meta.R
@@ -54,8 +54,6 @@ process_meta <- function(gt, gf, gg, gc, gsb, gcomp, glab, nx, panel.names, asp_
 			if (is.na(legend.outside)) legend.outside <- (nx > 1) && !any(vapply(gf[freescales], "[", logical(1), 1))
 		}
 		
-		if (is.na(title.snap.to.legend)) title.snap.to.legend <- legend.outside
-		
 		if (is.na(panel.show)) panel.show <- !is.na(panel.names[1])
 		if (legend.only) {
 			title <- rep("", nx)
