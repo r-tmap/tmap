376b906314c1ded22554def2ff398dd31e636459
Martijn Tennekes <mtennekes@gmail.com>
2017-12-09 21:29:26 +0100

    fixed syncing in tmap_arrange

--- R/tmap_arrange.R
+++ R/tmap_arrange.R
@@ -55,7 +55,7 @@ tmap_arrange <- function(..., ncol=NA, nrow=NA, sync=FALSE, asp=0, outer.margins
 		lfs <- lapply(tms, function(tm) {
 			tmap_leaflet(tm)
 		})
-		lfmv <- do.call(mapview::latticeView, c(lfs, list(ncol=ncol, sync=sync)))
+		lfmv <- do.call(mapview::latticeView, c(lfs, list(ncol=ncol, sync=ifelse(sync, "all", "none"))))
 		add_leaflet_titles(lfmv)
 	} else {
 		grid.newpage()
