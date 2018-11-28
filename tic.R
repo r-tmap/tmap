43716ecd8ba0c171d5a94b9fc075c75dc11059c8
mtennekes <mtennekes@gmail.com>
2018-11-28 10:52:56 +0100

    ...

--- tic.R
+++ tic.R
@@ -1,4 +1,4 @@
-add_package_checks(warnings_are_errors = FALSE)
+add_package_checks(error_on = "warning")
 
 get_stage("install") %>%
 	add_step(step_install_cran("lwgeom", configure.args="--without-liblwgeom")) %>% 
