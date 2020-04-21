49df30420e2b7a6ff580d4ddc92081e4469d577d
Martijn Tennekes <mtennekes@gmail.com>
2020-04-21 18:37:10 +0200

    try new tic

--- tic.R
+++ tic.R
@@ -1,6 +1,8 @@
 do_package_checks(error_on = "warning")
 
 get_stage("install") %>%
-	add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
+	add_code_step(remotes::install_github("r-spatial/sf")) %>%
+	add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom")) %>% 
+	add_code_step(remotes::install_github("mtennekes/tmaptools"))
 
 do_pkgdown(document = FALSE)
