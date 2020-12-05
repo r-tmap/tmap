7ba0ff4bc76d392627b875639c8e14011d3340ad
Martijn Tennekes <mtennekes@gmail.com>
2020-12-05 22:18:19 +0100

    working on stars

--- sandbox/main.R
+++ sandbox/main.R
@@ -1,19 +1,10 @@
-data(World)
-data(land)
-data(metro)
-data(rivers)
-
-
-suppressPackageStartupMessages(library(dplyr))
 library(stars)
+library(sf)
+library(data.table)
+library(pryr)
 
-prec_file = system.file("nc/test_stageiv_xyt.nc", package = "stars")
-(prec = read_ncdf(prec_file, curvilinear = c("lon", "lat"), ignore_bounds = TRUE))
-sf::read_sf(system.file("gpkg/nc.gpkg", package = "sf"), "nc.gpkg") %>%
-	st_transform(st_crs(prec)) -> nc # transform from NAD27 to WGS84
-nc_outline = st_union(st_geometry(nc))
-a = aggregate(prec, by = nc, FUN = max)
-a
+source("sandbox/test_data.R")
+
 
 
 ############ create tml
@@ -21,13 +12,37 @@ a
 tml = tm_shape(land) +
 tm_shape(World, name = "The World", is.main = TRUE) +
 tm_polygons("economy") +
-tm_shape(metro)
+tm_shape(metro) +
+tm_shape(a)
 
 
 
 ############# process shapes
 
 tmls = tm_element_list_sel(tml, "tm_shape")
+
+
+
+get_main_crs = function(tmls) {
+	is_main = vapply(tmls, function(tme) {
+		identical(tme$is.main, TRUE)
+	}, FUN.VALUE = logical(1))
+	
+	main_id = if (any(is_main)) which(is_main)[1L] else 1L
+	
+	tms_main = tmls[[main_id]]
+	
+	crs_main = tms_main$crs
+	if (is.null(crs_main)) crs_main = sf::st_crs(tms_main$shp)
+	crs_main
+}
+
+crs = get_main_crs(tmls)
+
+tmls = lapply(tmls, function(tms, crs) {
+	tms$crs = crs
+	tms
+}, crs = crs)
 
 
 x = tmls[[1]]
@@ -40,11 +55,10 @@ unit = x[[5]]
 shp_name = x[[6]]
 
 
+x$a=1
+
 do.call(.tmapShape, tmls[[1]])
 
-library(stars)
-library(sf)
-library(data.table)
 
 as.data.table(shp)
 shp = sf::st_as_sf(shp)
@@ -57,7 +71,11 @@ attr(x, "raster")
 get_raster(x)
 
 
+
+
 get_empty_raster = function(shp, shp_name) {
+	
+	
 	if (!has_raster(shp)) {
 		dimnms = dimnames(shp)
 		
@@ -77,15 +95,30 @@ get_empty_raster = function(shp, shp_name) {
 		}
 		
 		data = as.data.table(shp)
-		shp = geoms	
+		shp = geoms
+		
 	} else {
+		shp <- downsample_stars(shp, max.raster = 1e5)
+		if (sf::st_crs(shp) != crs) {
+			shp <- transwarp(shp, crs = crs, raster.warp = TRUE)
+		}
+		
+		
 		dims = st_dimensions(shp)
 		rst = attr(dims, "raster")
 		dimsxy = dims[names(dims) %in% rst$dimensions]
-		str(shp[[1]])
+
+		shp2 = st_set_dimensions(shp, rst$dimensions[1], values = 1L:nrow(shp))
+		shp3 = st_set_dimensions(shp2, rst$dimensions[2], values = 1L:ncol(shp))
 		
+		data = as.data.table(shp3, center = FALSE)
+		data[, tmapID__ := (y-1) * nrow(shp) + x]
+		data[, x]
 		
-		(r = st_as_stars(list(m = m), dimensions = dimsxy))
+		m = matrix(1L:(nrow(shp) * ncol(shp)), nrow = nrow(shp), ncol = ncol(shp))
+		
+		data = 
+		shp = st_as_stars(list(m = m), dimensions = dimsxy)
 	}
 	
 	
---
+++ sandbox/test_data.R
@@ -0,0 +1,59 @@
+suppressPackageStartupMessages(library(pryr))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(stars))
+suppressPackageStartupMessages(library(sf))
+
+
+data(World, package = "tmap")
+data(land, package = "tmap")
+data(metro, package = "tmap")
+data(rivers, package = "tmap")
+
+prec_file = system.file("nc/test_stageiv_xyt.nc", package = "stars")
+(prec = read_ncdf(prec_file, curvilinear = c("lon", "lat"), ignore_bounds = TRUE))
+sf::read_sf(system.file("gpkg/nc.gpkg", package = "sf"), "nc.gpkg") %>%
+	st_transform(st_crs(prec)) -> nc # transform from NAD27 to WGS84
+prec_nc = aggregate(prec, by = nc, FUN = max)
+
+tif = system.file("tif/L7_ETMs.tif", package = "stars") %>% read_stars()
+tif2 = c(tif,tif, along = 'testdim')
+
+weather = read_ncdf(system.file('nc/bcsd_obs_1999.nc', package = 'stars'))
+weather1 = st_set_dimensions(merge(weather), names = c('longitude','latitude','time','attributes'))
+weather2 = split(weather1, 'time')
+
+granule = system.file("sentinel/S2A_MSIL1C_20180220T105051_N0206_R051_T32ULE_20180221T134037.zip", package = "starsdata")
+s2 = paste0("SENTINEL2_L1C:/vsizip/", granule, "/S2A_MSIL1C_20180220T105051_N0206_R051_T32ULE_20180221T134037.SAFE/MTD_MSIL1C.xml:10m:EPSG_32632")
+gran = read_stars(s2, proxy = FALSE)
+grap_p = read_stars(s2, proxy = TRUE)
+
+rm(granule, s2)
+
+show_data = function() {
+	lst = ls(envir = .GlobalEnv)
+	cls = sapply(lst, function(o) class(get(o, envir = .GlobalEnv))[1])
+	size = sapply(lst, function(o) object_size(get(o, envir = .GlobalEnv)))
+	features = sapply(lst, function(o) {
+		o = get(o, envir = .GlobalEnv)
+		if (inherits(o, "sf")) {
+			as.character(length(o))
+		} else if (inherits(o, "stars")) {
+			paste0(length(o), " x (", paste(dim(o), collapse = " x "), ")")
+		} else {
+			""
+		}
+	})
+	hasr = sapply(lst, function(o) stars:::has_raster(get(o, envir = .GlobalEnv)))
+	
+	data.frame(name = names(size), size = format(unname(size), big.mark = ","),  class = cls, features = features, has_raster = hasr, row.names = NULL) %>% 
+		filter(class %in% c("sf", "stars")) %>% 
+		arrange(class, desc(size)) %>% 
+		print()
+	
+	print(mem_used())
+}
+
+show_data()
+gc()
+
+
