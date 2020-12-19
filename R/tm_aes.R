0e4ab0e5e284074a6c99a0a9781bd26153ab2b7a
Martijn Tennekes <mtennekes@gmail.com>
2020-12-19 18:28:17 +0100

    ...

--- R/tm_aes.R
+++ R/tm_aes.R
@@ -13,3 +13,20 @@ tm_aes_shape = function(shapes = c(16:21)) {
 	structure(as.list(environment()), class = c("tm_aes_shape", "tm_aes"))
 }
 
+tmapAesColorDiscrete = function(x1, setup) {
+	ci = classInt::classIntervals(x1, n = setup$n, style = setup$style)
+	pals::brewer.blues(setup$n)[classInt::findCols(ci)]
+}
+
+tmapAes2dSize = function(x1, setup) {
+	max = if (is.na(setup$max)) max(x1, na.rm = TRUE) else setup$max
+	x1 / max
+}
+
+tmapAesShape = function(x1, setup) {
+	if (is.numeric(x1)) x1 = as.integer(x1)
+	if (is.character(x1)) x1 = as.factor(x1)
+	if (is.factor(x1)) x1 = as.integer(x1)
+	#n = length(shapes)
+	shapes[x1]
+}
--- sandbox/main.R
+++ sandbox/main.R
@@ -30,91 +30,21 @@ profvis::profvis({
 	tmo = tmapObject(tmel)
 })
 
-
-tml = tmel[[2]]
-
-tmapLayer = function(tml) {
-	
-}
+mem_change({
+	tmo = tmapObject(tmel)
+})
 
 
 
+tmg = tmo[[1]]
+dt = tmg$tms$dt
 
+tml = tmg$tmls[[1]]
 
+dt2 = copy(dt)
 
+lapply(tml$aes.mapping, tml$aes.mapping.fun)
 
-
-tmapShps = lapply(tmls, function(ti) do.call(.tmapShape, ti))
-
-
-
-
-s = assign_values(tmls[[1]]$shp, dt = tmls[[1]]$dt, column = "cover")
-
-
-s = assign_values(tmls[[4]]$shp, dt = tmls[[4]]$dt, column = "cover")
-
-
-
-x = do.call(tmapShape, tmls[[1]])
-
-
-
-
-x = tmls[[1]]
-
-shp = x[[1]]
-is.main = x[[2]]
-crs = x[[3]]
-bbox = x[[4]]
-unit = x[[5]]
-shp_name = x[[6]]
-
-
-x$a=1
-
-do.call(.tmapShape, tmls[[1]])
-
-
-as.data.table(shp)
-shp = sf::st_as_sf(shp)
-
-has_raster(shp)
-r = attr(shp, "raster")
-
-attr(x, "raster")
-
-get_raster(x)
-
-
-
-
-get_empty_raster = function(shp, shp_name) {
-	
-	
-	
-	shp2 = shp
-	
-	
-	
-	shp2[[1]]
-	
-	
-}
-
-
-
-
-m = matrix(1:12, nrow = 3)
-data = data.frame(x = c(35, 27, 44), TMAP__ = c(1, 3, 8))
-
-m2 = matrix(NA, nrow = nrow(m), ncol = ncol(m))
-
-m[data$TMAP__] = data$x
-
-
-
-w = st_as_stars(World)
 
 
 
