2fe3b62b506e58346acd4c39844be62df938f9ca
mtennekes <mtennekes@gmail.com>
2021-09-30 18:26:10 +0200

    fixed bg.color

--- R/tmapGridShape.R
+++ R/tmapGridShape.R
@@ -11,11 +11,13 @@ tmapGridShape = function(bbx, facet_row, facet_col, facet_page, o) {
 	rowid = g$rows_facet_ids[facet_row]
 	colid = g$cols_facet_ids[facet_col]
 	
-	# innerRect = if (is.na(bgcol) && frame.lwd == 0) {
-	# 	NULL
-	# } else grid::rectGrob(gp=grid::gpar(col=frame.col, lwd = frame.lwd, fill = bgcol), name = "outer_frame")
-	
-	gtmap = grid::grobTree(NULL,#frame,
+	bg = if (is.na(o$frame) || is.na(o$bg.color)) {
+		NULL
+	} else {
+		rectGrob(gp=gpar(fill=o$bg.color, lwd=0, lineend="square"), name = "inner_rect")
+	}
+
+	gtmap = grid::grobTree(bg,
 						   vp = grid::vpStack(grid::viewport(layout.pos.col = colid, layout.pos.row = rowid, name = paste0("vp_facet_", rc_text)),
 						   				   grid::viewport(xscale = fbbx[c(1,3)], yscale = fbbx[c(2,4)], name = paste0("vp_map_", rc_text), clip = TRUE)), name = paste0("gt_facet_", rc_text))
 	
@@ -33,11 +35,6 @@ tmapGridOverlay = function(facet_row, facet_col, facet_page, o) {
 	
 	rc_text = frc(facet_row, facet_col)
 	
-	# rowid = g$rows_facet_ids[facet_row]
-	# colid = g$cols_facet_ids[facet_col]
-	
-	
-	bgcol = if (!is.na(o$bg.color)) o$bg.color else NA
 	frame.lwd = if (identical(o$frame, FALSE)) 0 else o$frame.lwd
 	frame.col = if (identical(o$frame, FALSE)) NA else if (identical(o$frame, TRUE)) o$attr.color else o$frame
 	
@@ -46,11 +43,11 @@ tmapGridOverlay = function(facet_row, facet_col, facet_page, o) {
 		pW = convertWidth(unit(1, "points"), unitTo = "npc", valueOnly = TRUE)*frame.lwd
 		if (o$frame.double.line) {
 			grid::grobTree(
-				rectGrob(width = 1-4*pW, height=1-4*pH, gp=gpar(col=bgcol, fill=bgcol, lwd=5*frame.lwd, lineend="square"), name = "outer_frame"),
+				rectGrob(width = 1-4*pW, height=1-4*pH, gp=gpar(col=frame.col, fill=NA, lwd=5*frame.lwd, lineend="square"), name = "outer_frame"),
 				rectGrob(gp=gpar(col=frame.col, fill=NA, lwd=3*frame.lwd, lineend="square"), name = "between_frame"),
 				rectGrob(width = 1-8*pW, height=1-8*pH, gp=gpar(col=frame.col, fill=NA, lwd=frame.lwd, lineend="square"), name = "inner_frame"))
 		} else {
-			rectGrob(gp=gpar(col=frame.col, fill=bgcol, lwd=frame.lwd, lineend="square"), name = "outer_frame")
+			rectGrob(gp=gpar(col=frame.col, fill=NA, lwd=frame.lwd, lineend="square"), name = "outer_frame")
 		}
 	} else NULL
 	
