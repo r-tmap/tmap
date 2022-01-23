6603038bf71d38827a5df4459c02a10e35d2cbb4
mtennekes <mtennekes@gmail.com>
2022-01-23 22:47:23 +0100

    legend working again: todo generalize legend positioning to positioning of every attribute (e.g. title and map compass)

--- R/legend_landscape.R
+++ R/legend_landscape.R
@@ -10,6 +10,54 @@ tmapGridLegPlot = function(...) {
 	UseMethod("tmapGridLegPlot")
 }
 
+#' @method tmapGridLegHeight tm_attr_title
+#' @export
+tmapGridLegHeight.tm_attr_title = function(leg, o) {
+	titleS = if (leg$title == "") 0 else leg$title.size
+	titleP = leg$title.padding[c(3,1)] * titleS * o$lin
+	titleH = titleS * o$lin
+	leg$Hin = sum(titleP[1], titleH, titleP[2])
+	leg
+}
+
+#' @method tmapGridLegWidth tm_attr_title
+#' @export
+tmapGridLegWidth.tm_attr_title = function(leg, o) {
+	titleS = if (leg$title == "") 0 else leg$title.size
+	titleP = leg$title.padding[c(2,4)] * titleS * o$lin
+	titleW = titleS * strwidth(leg$title, units = "inch", family = leg$title.fontfamily, font = fontface2nr(leg$title.fontface)) * o$lin
+	leg$Win = sum(titleP[1], titleW, titleP[2])
+	leg
+}
+
+
+#' @method tmapGridLegPlot tm_attr_title
+#' @export
+tmapGridLegPlot.tm_attr_title = function(leg, o) {
+	textS = leg$text.size * leg$scale
+	titleS = if (leg$title == "") 0 else leg$title.size * leg$scale
+	
+	if (leg$title.just == "left") {
+		grTitle = grid::textGrob(leg$title, x = grid::unit(leg$title.padding[2] * titleS * o$lin, units = "inch"), just = "left", gp = grid::gpar(cex = titleS))
+	} else if (leg$title.just == "right") {
+		grTitle = grid::textGrob(leg$title, x = grid::unit(1, "npc") - grid::unit(leg$title.padding[4] * titleS * o$lin, units = "inch"), just = "right", gp = grid::gpar(cex = titleS))
+	} else {
+		grTitle = grid::textGrob(leg$title, x = 0.5, just = "center", gp = grid::gpar(cex = titleS))
+	}
+	
+	if (getOption("tmap.design.mode")) {
+		grDesign = grid::rectGrob(gp=gpar(fill=NA,col="red", lwd=2))
+	} else {
+		grDesign = NULL
+	}
+	
+	g = do.call(grid::grobTree, c(list(grTitle), grDesign, list(vp = vp)))
+	
+	g
+	
+}
+
+
 
 #' @method tmapGridLegHeight tm_legend_standard_landscape
 #' @export
