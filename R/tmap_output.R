fa9547585706cac853df9a299d6d5bd0f3215ed3
robinlovelace <rob00x@gmail.com>
2023-07-27 16:05:10 +0100

    Put tmap_leaflet above

--- R/tmap_output.R
+++ R/tmap_output.R
@@ -1,17 +1,3 @@
-#' @rdname tmap_leaflet
-#' @name tmap_grid
-#' @export
-tmap_grid = function(x,
-					 show = FALSE,
-					 ...) {
-	current_mode = getOption("tmap.mode")
-	on.exit({
-		options(tmap.mode = current_mode)
-	})
-	options(tmap.mode = "plot")
-	print.tmap(x, show=show, ...)
-}
-
 #' Export tmap to the format of the used graphics mode
 #' 
 #' Export tmap to the format of the used graphics mode. `tmap_grid` returns a \code{\link[grid::grob]{grob}} object (\code{"plot\" mode}) and `tmap_leaflet` a \code{\link{leaflet::leaflet}} object (\code{"view"} mode).
@@ -19,9 +5,9 @@ tmap_grid = function(x,
 #' @param x description
 #' @param show show the map?
 #' @param ... arguments passed on the \code{\link{print.tmap}}
-#' @rdname tmap_leaflet
-#' @name tmap_leaflet
+#' 
 #' @return `tmap_grid` returns a \code{\link[grid::grob]{grob}} object (\code{"plot\" mode}) and `tmap_leaflet` a \code{\link{leaflet::leaflet}} object (\code{"view"} mode). In case small multiples are shown, a list is returned.
+#' 
 #' @export
 #' @examples
 #' map = tm_shape(World) + tm_polygons()
@@ -37,5 +23,18 @@ tmap_leaflet = function(x,
 		options(tmap.mode = current_mode)
 	})
 	options(tmap.mode = "view")
+	print.tmap(x, show=show, ...)
+}
+
+#' @rdname tmap_leaflet
+#' @export
+tmap_grid = function(x,
+					 show = FALSE,
+					 ...) {
+	current_mode = getOption("tmap.mode")
+	on.exit({
+		options(tmap.mode = current_mode)
+	})
+	options(tmap.mode = "plot")
 	print.tmap(x, show=show, ...)
 }
