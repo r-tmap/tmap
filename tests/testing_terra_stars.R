93d2d9407353a86f4e22005e39f649f6328bc583
olivroy <olivierroy71@hotmail.com>
2023-09-14 16:42:06 -0400

    [testing] Add try before failing tests to convert to warnings + add tryCatch to  fix a bug introduced by https://github.com/r-tmap/tmap/commit/a713947b663f6ea36847f8188646dde1d5a2b1ae

--- tests/testing_terra_stars.R
+++ tests/testing_terra_stars.R
@@ -13,10 +13,10 @@ land_terra = rast(land)
 names(land_terra) = names(land) # bug in terra?
 
 tm_shape(land_stars) + tm_raster("trees")
-tm_shape(land_stars) + tm_raster("treess")
+try(tm_shape(land_stars) + tm_raster("treess"))
 
 tm_shape(land_terra) + tm_raster("trees")
-tm_shape(land_terra) + tm_raster("treess")
+try(tm_shape(land_terra) + tm_raster("treess"))
 
 
 tm_shape(landsat_stars) + tm_raster("landsat.tif", col.free = FALSE)
@@ -53,10 +53,9 @@ multi_rast = rast(multi_raster_file)
 
 tm_shape(multi_rast) +
 	tm_rgb(tm_mv("landsat_4", "landsat_3", "landsat_2"), col.scale = tm_scale_rgb(maxValue = 31961)) 
-#> Error: [subset] no (valid) layer selected
 
-tm_shape(multi_rast[[3:1]]) +
-	tm_rgb() 
+try(tm_shape(multi_rast[[3:1]]) +
+	tm_rgb())
 
 
 # idea: tm_attr to specify an attribute as mv
