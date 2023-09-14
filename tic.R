9f0e06ec7bda3309bcb3429193a54dc19da5f22a
olivroy <olivierroy71@hotmail.com>
2023-09-14 16:39:26 -0400

    Remove tic.R

--- tic.R
+++
@@ -1,6 +0,0 @@
-# do_package_checks(error_on = "warning")
-# 
-# get_stage("install") %>%
-# 	#add_code_step(remotes::install_github("r-spatial/sf")) %>%
-# 	add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
-# 
