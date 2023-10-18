f0f00d63d9b19ad4f6d14ade3024d5a208174069
mtennekes <mtennekes@gmail.com>
2023-10-18 09:22:26 +0200

    #794

--- R/tmapGridComp.R
+++ R/tmapGridComp.R
@@ -562,6 +562,22 @@ tmapGridLegPlot_text = function(comp, o, fH, fW) {
 	
 }
 
+
+correct_nlines = function(n) {
+	# Linear model applied based on this empirical data:
+	# (results may depend on device)
+	#
+	# y = sapply(1:50, function(i) {
+	# 	s = paste(rep("text", i), collapse = "\n")
+	# 	convertHeight(stringHeight(s), "inch", valueOnly = TRUE)
+	# })
+	# df = data.frame(x = 1:50, y = y / 0.2) #0.2 is the lineheight (par "cin")
+	# lm(y~x, df)
+	# 
+	-.6035 + n * 1.2
+}
+
+
 tmapGridCompHeight_text = function(comp, o) {
 	textS = if (comp$text == "") 0 else comp$size #* o$scale
 	textP = comp$padding[c(3,1)] * textS * o$lin
@@ -569,7 +585,9 @@ tmapGridCompHeight_text = function(comp, o) {
 	
 	nlines = number_text_lines(comp$text)
 	
-	comp$Hin = sum(textP[1], textH * nlines, textP[2])
+	nlines2 = correct_nlines(nlines)
+	
+	comp$Hin = sum(textP[1], textH * nlines2, textP[2])
 	comp
 }
 
@@ -593,17 +611,32 @@ wrapText = function(txt, nlines) {
 }
 
 
+
+
 tmapGridCompWidth_text = function(comp, o) {
 	textS = if (comp$text == "") 0 else comp$size #* o$scale
 	textP = comp$padding[c(2,4)] * textS * o$lin
 	textW = textS * graphics::strwidth(comp$text, units = "inch", family = comp$fontfamily, font = fontface2nr(comp$fontface))
 	
 	w = sum(textP[1], textW, textP[2])
+	
 	if (!is.na(comp$width)) {
-		approxNumL = min(20, round(w / (comp$width * textS * o$lin)))
-		comp$text = wrapText(comp$text, approxNumL)
-		comp$Win = comp$width * textS * o$lin
+		textPgs = strsplit(comp$text, "\n")[[1]]
+		text2 = do.call(paste, c(lapply(textPgs, function(p) {
+			textW = textS * graphics::strwidth(p, units = "inch", family = comp$fontfamily, font = fontface2nr(comp$fontface))
+			w = sum(textP[1], textW, textP[2])
+			nlines = round(w / (comp$width * textS * o$lin))
+			wrapText(p, nlines)
+		}), list(sep = "\n")))
+		
+		textW2 = textS * graphics::strwidth(text2, units = "inch", family = comp$fontfamily, font = fontface2nr(comp$fontface))
+		w2 = sum(textP[1], textW2, textP[2])
+		
+		#approxNumL = min(20, round(w / (comp$width * textS * o$lin)))
+		comp$text = text2
+		comp$Win = w2
 	} else {
+		#comp$nlines = length(comp$textPgs)
 		comp$Win = w
 	}
 	comp
