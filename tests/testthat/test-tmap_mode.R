b58cbcf7bc069ddb4a4f052c6d52b79961cf63e4
olivroy <52606734+olivroy@users.noreply.github.com>
2024-08-21 13:42:12 -0400

    Update tests/testthat/test-tmap_mode.R

--- tests/testthat/test-tmap_mode.R
+++ tests/testthat/test-tmap_mode.R
@@ -1,6 +1,5 @@
 test_that("tmap_mode is plot by default.", {
 	skip_on_cran()
-	tmap_mode("plot")
-	expect_message(tmap_mode(), "current tmap mode is \"plot\"")
+	expect_message(tmap_mode(), "Current tmap mode is \"plot\"")
 })
 
