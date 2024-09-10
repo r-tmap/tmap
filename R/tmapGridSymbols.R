69c48a1a83807637655af8dec131362555b35851
mtennekes <mtennekes@gmail.com>
2024-09-10 17:01:39 +0200

    fixed #934

--- R/tmapGridSymbols.R
+++ R/tmapGridSymbols.R
@@ -4,40 +4,40 @@
 tmapGridSymbols = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page, id, pane, group, o, ...) {
 	args = list(...)
 	rc_text = frc(facet_row, facet_col)
-	
+
 	g = get("g", .TMAP_GRID)
-	
+
 	# COPIED FROM tmapGridText -> function
 	# calculate native per line
 	wIn = g$colsIn[g$cols_facet_ids[facet_col]]
 	hIn = g$rowsIn[g$rows_facet_ids[facet_row]]
-	
+
 	wNative = bbx[3] - bbx[1]
 	hNative = bbx[4] - bbx[2]
-	
+
 	xIn = wNative / wIn
 	yIn = hNative / hIn
-	
+
 	lineIn = convertHeight(unit(1, "lines"), "inch", valueOnly = TRUE)
-	
-	
+
+
 	res = select_sf(shpTM, dt[!is.na(dt$size), ])
 	shp = res$shp
 	dt = res$dt
-	
+
 	gp = impute_gp(gp, dt)
-	gp = swap_pch_15_20(gp)	
-	
+	gp = swap_pch_15_20(gp)
+
 	#gp = get_pch_1000p(gp)
-	
+
 	gp = rescale_gp(gp, o$scale_down * o$scale)
-	
+
 	#gp = gp_to_gpar(gp, sel = "all")
-	
+
 	diffAlpha = !any(is.na(c(gp$fill_alpha, gp$col_alpha))) && !(length(gp$fill_alpha) == length(gp$col_alpha) && all(gp$fill_alpha == gp$col_alpha))
-	
+
 	coords = sf::st_coordinates(shp)
-	
+
 	# in case shp is a multipoint (point.per == "segment"), expand gp:
 	if (ncol(coords) == 3L) {
 		ndt = nrow(dt)
@@ -50,27 +50,27 @@ tmapGridSymbols = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page,
 				gpi
 			}
 		})
-		coords = coords[,1:2]
+		coords = coords[, 1:2, drop=FALSE]
 	}
-	
-	
+
+
 	if (diffAlpha) {
 		gp1 = gp_to_gpar(gp, sel = "fill", o = o, type = "symbols")
 		gp2 = gp_to_gpar(gp, sel = "col", o = o, type = "symbols")
-		
+
 		grb1 = grid::pointsGrob(x = grid::unit(coords[,1], "native"), y = grid::unit(coords[,2], "native"), pch = gp$shape, size = grid::unit(gp$size, "lines"), gp = gp1, name = paste0("symbols_", id))
 		grb2 = grid::pointsGrob(x = grid::unit(coords[,1], "native"), y = grid::unit(coords[,2], "native"), pch = gp$shape, size = grid::unit(gp$size, "lines"), gp = gp2, name = paste0("symbols_borders_", id))
 		grb = grid::grobTree(grb1, grb2)
 	} else {
 		gp = gp_to_gpar(gp, sel = "all", o = o, type = "symbols")
-		
+
 		if (any(!is.na(gp$shape) & gp$shape>999)) {
 			shapeLib = get("shapeLib", envir = .TMAP)
 			justLib = get("justLib", envir = .TMAP)
-			
+
 			grobs <- lapply(1:length(gp$shape), function(i) {
 				shi = gp$shape[[i]]
-				
+
 				just = if (is.na(args$just[1])) c(0.5, 0.5) else args$just
 				jst = if (!is.na(shi) && shi>999) {
 					js = justLib[[shi-999]]
@@ -82,24 +82,24 @@ tmapGridSymbols = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page,
 						just
 					}
 				} else just
-				
+
 				justs.x = jst[1]
 				justs.y = jst[2]
 				justx = xIn * lineIn * (justs.x-.5)
 				justy = yIn * lineIn * (justs.y-.5)
-				
-				
+
+
 
 				if (!is.na(shi) && shi>999) {
 					size = gp$size[i]*9/10 * args$icon.scale
 					lwdid = if (length(gp$lwd) == 1L) 1 else i
-					grbs <- gList(shapeLib[[shi-999]])	
-					gTree(children=grbs, vp=viewport(x=grid::unit(coords[i,1] + justx * size, "native"), 
+					grbs <- gList(shapeLib[[shi-999]])
+					gTree(children=grbs, vp=viewport(x=grid::unit(coords[i,1] + justx * size, "native"),
 													 y=grid::unit(coords[i,2] + justy * size, "native"),
 													 width=unit(size, "lines"),
 													 height=unit(size, "lines")))
 				} else {
-					pointsGrob(x=grid::unit(coords[i,1] + justx * gp$size[i], "native"), 
+					pointsGrob(x=grid::unit(coords[i,1] + justx * gp$size[i], "native"),
 							   y=grid::unit(coords[i,2] + justx * gp$size[i], "native"),
 							   size=unit(gp$size[i], "lines"),
 							   pch=shi,
@@ -110,20 +110,20 @@ tmapGridSymbols = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page,
 		} else {
 			grb = grid::pointsGrob(x = grid::unit(coords[,1], "native"), y = grid::unit(coords[,2], "native"), pch = gp$shape, size = grid::unit(gp$size, "lines"), gp = gp, name = paste0("symbols_", id))
 		}
-		
-		
+
+
 	}
-	
-	
+
+
 	gts = get("gts", .TMAP_GRID)
 	gt = gts[[facet_page]]
-	
+
 	gt_name = paste0("gt_facet_", rc_text)
-	
+
 	gt = grid::addGrob(gt, grb, gPath = grid::gPath(gt_name))
-	
+
 	gts[[facet_page]] = gt
 	assign("gts", gts, envir = .TMAP_GRID)
-	NULL	
-	
+	NULL
+
 }
--- R/tmapLeaflet_layers.R
+++ R/tmapLeaflet_layers.R
@@ -1,6 +1,6 @@
 submit_labels = function(labels, cls, pane, group) {
 	layerIds = get("layerIds", envir = .TMAP_LEAFLET)
-	
+
 	if (length(layerIds)) {
 		labels = local({
 			labels_all = unlist(lapply(layerIds, function(l) l$Lid), use.names = FALSE)
@@ -8,7 +8,7 @@ submit_labels = function(labels, cls, pane, group) {
 			labels_all = gsub("_", ".", labels_all,fixed = TRUE)
 			labels_all = make.names(c(labels_all, labels), unique = TRUE)
 			labels_all = gsub(".", "_", labels_all,fixed = TRUE)
-			labels_all[(pos + 1): length(labels_all)]	
+			labels_all[(pos + 1): length(labels_all)]
 		})
 	} else {
 		labels = make.names(labels, unique = TRUE)
@@ -16,7 +16,7 @@ submit_labels = function(labels, cls, pane, group) {
 	}
 
 	layerIds = c(layerIds, list(list(name = pane, type = cls, group = group, Lid = labels)))
-	
+
 	assign("layerIds", layerIds, envir = .TMAP_LEAFLET)
 	labels
 }
@@ -26,26 +26,26 @@ submit_labels = function(labels, cls, pane, group) {
 #' @rdname tmap_internal
 tmapLeafletPolygons = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, facet_row, facet_col, facet_page, id, pane, group, o, ...) {
 	lf = get_lf(facet_row, facet_col, facet_page)
-	
+
 	rc_text = frc(facet_row, facet_col)
-	
+
 	res = select_sf(shpTM, dt)
 	shp = res$shp
 	dt = res$dt
-	
+
 	if (is.null(pdt)) {
 		popups = NULL
 	} else {
 		pdt = pdt[match(dt$tmapID__, pdt$tmapID__)][, tmapID__ := NULL]
 		popups = view_format_popups(id = idt, titles = names(pdt), values = pdt, format = popup.format)
 	}
-	
-	
+
+
 	gp = impute_gp(gp, dt)
 	gp = rescale_gp(gp, o$scale_down)
-	
+
 	opt = leaflet::pathOptions(interactive = TRUE, pane = pane)
-	
+
 	if (is.null(idt)) idt = submit_labels(dt$tmapID__, "polygons", pane, group)
 
 	if (o$use.WebGL) {
@@ -53,15 +53,15 @@ tmapLeafletPolygons = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, 
 		shp3 = suppressWarnings(sf::st_cast(shp2, "POLYGON"))
 		gp3 = lapply(gp, function(gpi) {if (length(gpi) == 1) gpi else gpi[shp3$id]})
 		popups2 = popups[shp3$id]
-		lf %>% 
-			leafgl::addGlPolygons(data = shp3, layerId = idt, color = gp3$col, opacity = gp3$col_alpha, fillColor = gp3$fill, fillOpacity = gp3$fill_alpha, weight = gp3$lwd, group = group, pane = pane, popup = popups2) %>% 
+		lf %>%
+			leafgl::addGlPolygons(data = shp3, layerId = idt, color = gp3$col, opacity = gp3$col_alpha, fillColor = gp3$fill, fillOpacity = gp3$fill_alpha, weight = gp3$lwd, group = group, pane = pane, popup = popups2) %>%
 			assign_lf(facet_row, facet_col, facet_page)
 	} else {
-		lf %>% 
-			leaflet::addPolygons(data = shp, layerId = idt, label = hdt, color = gp$col, opacity = gp$col_alpha, fillColor = gp$fill, fillOpacity = gp$fill_alpha, weight = gp$lwd, options = opt, group = group, dashArray = lty2dash(gp$lty), popup = popups) %>% 
+		lf %>%
+			leaflet::addPolygons(data = shp, layerId = idt, label = hdt, color = gp$col, opacity = gp$col_alpha, fillColor = gp$fill, fillOpacity = gp$fill_alpha, weight = gp$lwd, options = opt, group = group, dashArray = lty2dash(gp$lty), popup = popups) %>%
 			assign_lf(facet_row, facet_col, facet_page)
 	}
-	NULL	
+	NULL
 }
 
 
@@ -72,16 +72,16 @@ lty2dash = function(lty) {
 		unname(tab[lty])
 	} else {
 		are_letters = (suppressWarnings(!is.na(as.numeric(lty))))
-		
+
 		if (!all(are_letters | are_words)) {
 			stop("Incorrect lty specification: ", lty[which(!are_letters & !are_words)[1]])
 		} else {
 			lty[are_words] = unname(tab[lty[are_words]])
-			lty[are_letters] = vapply(strsplit(lty[are_letters], ""), FUN = function(x) paste(x, collapse = " "), FUN.VALUE = character(1)) 
+			lty[are_letters] = vapply(strsplit(lty[are_letters], ""), FUN = function(x) paste(x, collapse = " "), FUN.VALUE = character(1))
 		}
 		lty
 	}
-	
+
 }
 
 #' @export
@@ -89,52 +89,52 @@ lty2dash = function(lty) {
 #' @rdname tmap_internal
 tmapLeafletLines = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, facet_row, facet_col, facet_page, id, pane, group, o) {
 	lf = get_lf(facet_row, facet_col, facet_page)
-	
+
 	rc_text = frc(facet_row, facet_col)
-	
+
 	res = select_sf(shpTM, dt)
 	shp = res$shp
 	names(shp) = NULL # also required for other layers?
 	dt = res$dt
-	
+
 	if (is.null(pdt)) {
 		popups = NULL
 	} else {
 		pdt = pdt[match(dt$tmapID__, pdt$tmapID__)][, tmapID__ := NULL]
 		popups = view_format_popups(id = idt, titles = names(pdt), values = pdt, format = popup.format)
 	}
-	
+
 	gp = impute_gp(gp, dt)
 	gp = rescale_gp(gp, o$scale_down)
-	
+
 	opt = leaflet::pathOptions(interactive = TRUE, pane = pane)
 	if (is.null(idt)) idt = submit_labels(dt$tmapID__, "lines", pane, group)
-	
+
 	if (o$use.WebGL) {
 		shp2 = sf::st_sf(id = seq_along(shp), geom = shp)
 		shp3 = suppressWarnings(sf::st_cast(shp2, "LINESTRING"))
 		gp3 = lapply(gp, function(gpi) {if (length(gpi) == 1) gpi else gpi[shp3$id]})
-		lf %>% 
-			leafgl::addGlPolylines(data = shp3, color = gp3$col, opacity = gp3$col_alpha, weight = gp3$lwd, pane = pane, group = group, layerId = idt) %>% 
+		lf %>%
+			leafgl::addGlPolylines(data = shp3, color = gp3$col, opacity = gp3$col_alpha, weight = gp3$lwd, pane = pane, group = group, layerId = idt) %>%
 			assign_lf(facet_row, facet_col, facet_page)
 	} else {
-		
-		lf %>% 
-			leaflet::addPolylines(data = shp, layerId = idt, label = hdt, color = gp$col, opacity = gp$col_alpha, weight = gp$lwd, group = group, options = opt, dashArray = lty2dash(gp$lty), popup = popups) %>% 
+
+		lf %>%
+			leaflet::addPolylines(data = shp, layerId = idt, label = hdt, color = gp$col, opacity = gp$col_alpha, weight = gp$lwd, group = group, options = opt, dashArray = lty2dash(gp$lty), popup = popups) %>%
 			assign_lf(facet_row, facet_col, facet_page)
-		
+
 	}
-	NULL	
+	NULL
 }
 
-makeSymbolIcons2  = function (shape, color, fillColor = color, opacity, fillOpacity = opacity, 
-		  strokeWidth = 1, width, height = width, ...) 
+makeSymbolIcons2  = function (shape, color, fillColor = color, opacity, fillOpacity = opacity,
+		  strokeWidth = 1, width, height = width, ...)
 {
-	symbols <- Map(leaflegend::makeSymbol, shape = shape, width = width, 
-				   height = height, color = color, fillColor = fillColor, 
-				   opacity = opacity, fillOpacity = fillOpacity, `stroke-width` = strokeWidth, 
+	symbols <- Map(leaflegend::makeSymbol, shape = shape, width = width,
+				   height = height, color = color, fillColor = fillColor,
+				   opacity = opacity, fillOpacity = fillOpacity, `stroke-width` = strokeWidth,
 				   ...)
-	leaflet::icons(iconUrl = unname(symbols), iconAnchorX = width/2 + strokeWidth, 
+	leaflet::icons(iconUrl = unname(symbols), iconAnchorX = width/2 + strokeWidth,
 				   iconAnchorY = height/2 + strokeWidth)
 }
 
@@ -144,9 +144,9 @@ makeSymbolIcons2  = function (shape, color, fillColor = color, opacity, fillOpac
 tmapLeafletSymbols = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, facet_row, facet_col, facet_page, id, pane, group, o, ...) {
 	args = list(...)
 	lf = get_lf(facet_row, facet_col, facet_page)
-	
+
 	rc_text = frc(facet_row, facet_col)
-	
+
 	res = select_sf(shpTM, dt[!is.na(dt$size), ])
 	shp = res$shp
 	dt = res$dt
@@ -155,15 +155,15 @@ tmapLeafletSymbols = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, f
 		popups = NULL
 	} else {
 		pdt = pdt[match(dt$tmapID__, pdt$tmapID__)][, tmapID__ := NULL]
-	
+
 		popups = view_format_popups(id = idt, titles = names(pdt), values = pdt, format = popup.format)
 	}
-	
+
 	gp = impute_gp(gp, dt)
 	gp = rescale_gp(gp, o$scale_down)
 
 	coords = sf::st_coordinates(shp)
-	
+
 	# in case shp is a multipoint (point.per == "segment"), expand gp:
 	if (ncol(coords) == 3L) {
 		ndt = nrow(dt)
@@ -176,50 +176,50 @@ tmapLeafletSymbols = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, f
 				gpi
 			}
 		})
-		coords = coords[,1:2]
+		coords = coords[, 1:2, drop=FALSE]
 		tmapID_ids = match(shpTM$tmapID_expanded, shpTM$tmapID)
 		if (!is.null(idt)) idt = submit_labels(dt$tmapID__[tmapID_ids], "text", pane, group)
 	} else {
 		if (!is.null(idt)) idt = submit_labels(dt$tmapID__, "text", pane, group)
 	}
-	
+
 	opt = leaflet::pathOptions(interactive = TRUE, pane = pane)
-	
-	gp2 = gp_to_lpar(gp, mfun = "Symbols", size_factor = 14) # 14 based on similarity with plot mode and consistency with tmap3 
+
+	gp2 = gp_to_lpar(gp, mfun = "Symbols", size_factor = 14) # 14 based on similarity with plot mode and consistency with tmap3
 	#gp = gp2leafgp(gp)
 	names(gp2)[names(gp2) == 'stroke-width'] = "strokeWidth"
 	gp2$baseSize = 20
-	
+
 	#po(sort(gp2$width, decreasing = T))
-	
-	
-	
+
+
+
 	if (o$use.WebGL) {
 		vary = vapply(dt, function(x)any(x!=x[1]), FUN.VALUE = logical(1))[c("col", "shape", "lwd", "lty", "fill_alpha", "col_alpha")]
 		if (any(vary)) warning("WegGL enabled: the only supported visual variables are: fill and size. The visual variable(s) ", paste(names(vary)[vary], collapse = ", "), " are not supported. Disable WebGL to show them.", call. = FALSE)
-		lf %>% leafgl::addGlPoints(sf::st_sf(shp), fillColor = gp2$fillColor, radius = gp2$width, fillOpacity = gp2$fillOpacity[1], pane = pane, group = group) %>%  
+		lf %>% leafgl::addGlPoints(sf::st_sf(shp), fillColor = gp2$fillColor, radius = gp2$width, fillOpacity = gp2$fillOpacity[1], pane = pane, group = group) %>%
 			assign_lf(facet_row, facet_col, facet_page)
 	} else {
 
 		sn = suppressWarnings(as.numeric(gp2$shape))
-		
+
 		is_num = !is.na(sn)
 		sid = which(is_num)
 		nid = which(!is_num)
-		
+
 		gp2$shape[sid] = "circle" # as dummy
 		symbols = do.call(makeSymbolIcons2, gp2)
-		
+
 		symbols$iconWidth = rep(NA, length(symbols$iconUrl))
 		symbols$iconHeight = rep(NA, length(symbols$iconUrl))
-		
+
 		if (length(sid)) {
 			iconLib <- get("shapeLib", envir = .TMAP)[sn[sid]-999]
 			symbols_icons <- merge_icons(iconLib)
 			size = gp2$width[sid] / gp2$baseSize
-			
+
 			size[sid] = size[sid] * args$icon.scale/3
-			
+
 			for (i in seq_along(sid)) {
 				symbols$iconUrl[sid[i]] = symbols_icons$iconUrl[i]
 				symbols$iconWidth[sid[i]] <- symbols_icons$iconWidth[i] * size[i]
@@ -233,24 +233,24 @@ tmapLeafletSymbols = function(shpTM, dt, pdt, popup.format, hdt, idt, gp, bbx, f
 		}
 
 
-		lf %>% leaflet::addMarkers(lng = coords[, 1], lat = coords[, 2], 
-								  icon = symbols, group = group, layerId = idt, label = hdt, popup = popups, options = opt) %>% 
+		lf %>% leaflet::addMarkers(lng = coords[, 1], lat = coords[, 2],
+								  icon = symbols, group = group, layerId = idt, label = hdt, popup = popups, options = opt) %>%
 			assign_lf(facet_row, facet_col, facet_page)
-		
+
 	}
-	
-	
+
+
 	# if (o$use.WebGL) {
-	# 	lf %>% 
-	# 		leafgl::addGlPoints(sf::st_sf(shp), fillColor = gp$fill, radius = gp$size*10, fillOpacity = gp$fill_alpha, color = gp$col, opacity = gp$color_alpha, weight = gp$lwd, pane = pane, group = group) %>% 
+	# 	lf %>%
+	# 		leafgl::addGlPoints(sf::st_sf(shp), fillColor = gp$fill, radius = gp$size*10, fillOpacity = gp$fill_alpha, color = gp$col, opacity = gp$color_alpha, weight = gp$lwd, pane = pane, group = group) %>%
 	# 		assign_lf(facet_row, facet_col, facet_page)
 	# } else {
-	# 	lf %>% 
-	# 		leaflet::addCircleMarkers(lng = coords[, 1], lat = coords[, 2], fillColor = gp$fill, radius = gp$size*4, fillOpacity = gp$fill_alpha, color = gp$col, opacity = gp$color_alpha, weight = gp$lwd, group = group, options = opt) %>% 
+	# 	lf %>%
+	# 		leaflet::addCircleMarkers(lng = coords[, 1], lat = coords[, 2], fillColor = gp$fill, radius = gp$size*4, fillOpacity = gp$fill_alpha, color = gp$col, opacity = gp$color_alpha, weight = gp$lwd, group = group, options = opt) %>%
 	# 		assign_lf(facet_row, facet_col, facet_page)
 	# }
-	
-	NULL	
+
+	NULL
 }
 
 split_alpha_channel <- function(x, alpha) {
@@ -268,107 +268,107 @@ split_alpha_channel <- function(x, alpha) {
 #' @keywords internal
 #' @rdname tmap_internal
 tmapLeafletRaster = function(shpTM, dt, gp, pdt, popup.format, hdt, idt, bbx, facet_row, facet_col, facet_page, id, pane, group, o, ...) {
-	
+
 	rc_text = frc(facet_row, facet_col)
-	
-	
+
+
 	#bb_target <- bbx #attr(shp, "bbox")
 	#bb_real <- bbx #sf::st_bbox(shp)
-	
+
 	shp = shpTM$shp
 	tmapID = shpTM$tmapID
-	
+
 	if (is_regular_grid(shp)) {
-		
+
 		tid = intersect(tmapID, dt$tmapID__)
-		
+
 		color = rep(NA, length(tmapID)) # NA
-		
+
 		sel = which(tmapID %in% tid)
 		tid2 = tmapID[sel]
-		
+
 		color[sel] = dt$col[match(tid2, dt$tmapID__)]
-		
+
 
 		#color = rep("#FFFFFF", length(tmapID))
 		#color[match(dt$tmapID__, tmapID)] = dt$color
-		
+
 		pal <- na.omit(unique(color))
 		pal <- pal[substr(pal, 8,10)!="00"] ## remove transparant colors
-		
+
 		if (!length(pal)) return(NULL)
-		
+
 		res <- split_alpha_channel(pal, alpha = 1)
 		pal_col <- res$col
 		pal_opacity <- if (length(res$opacity) == 0L) 0 else max(res$opacity)
-		
+
 		if ("col_alpha" %in% names(dt)) pal_opacity = max(dt$col_alpha)
-		
-		
+
+
 		col_ids <- match(color, pal)
-		
+
 		m <- matrix(col_ids, ncol = ncol(shp))
-		
+
 		#matrix(color, ncol = ncol(shp))
-		
+
 		#m <- matrix(tmapID, ncol = ncol(shp))
-		
-		
+
+
 		#m = tmapID
-		
+
 		#m[1,5] = 4
-		
+
 		shp2 = stars::st_as_stars(m, dimensions = shp)
 
 		lf = get_lf(facet_row, facet_col, facet_page)
-		
+
 		opts = leaflet::gridOptions(pane = pane)
-		
-		lf %>% 
-			leafem::addStarsImage(shp2, band = 1, colors = pal_col, opacity = pal_opacity, group = group, options = opts) %>% 
+
+		lf %>%
+			leafem::addStarsImage(shp2, band = 1, colors = pal_col, opacity = pal_opacity, group = group, options = opts) %>%
 			assign_lf(facet_row, facet_col, facet_page)
 	} else {
 		#shp2 = stars::st_as_stars(list(values = tmapID), dimensions = shp)
 		#shpTM = shapeTM(sf::st_geometry(sf::st_as_sf(shp2)), as.vector(tmapID))
-		
+
 		m = matrix(tmapID, nrow = nrow(shp), ncol = ncol(shp))
 		shp2 = structure(list(tmapID = m), class = "stars", dimensions = shp)
-		
+
 		shp3 = sf::st_geometry(sf::st_as_sf(shp2))
-		
+
 		crs = get_option_class(tmap_options_mode("view")$crs, "sf")
-		
+
 		shpTM = shapeTM(sf::st_transform(shp3, crs), tmapID)
-		
-		
+
+
 		gp$lty = "solid"
 		tmapLeafletPolygons(shpTM, dt, pdt, popup.format = NULL, hdt = NULL, idt = NULL, gp, bbx, facet_row, facet_col, facet_page, id, pane, group, o)
 		#grid.shape(s, gp=gpar(fill=color, col=NA), bg.col=NA, i, k)
 	}
 	NULL
-} 
+}
 
 #' @export
 #' @keywords internal
 #' @rdname tmap_internal
 tmapLeafletText = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page, id, pane, group, o, ...) {
 	args = list(...)
-	
+
 	lf = get_lf(facet_row, facet_col, facet_page)
-	
+
 	rc_text = frc(facet_row, facet_col)
-	
+
 	res = select_sf(shpTM, dt)
 	shp = res$shp
 	dt = res$dt
-	
+
 	#shadow = gp$shadow
-	
+
 	gp = impute_gp(gp, dt)
 	gp = rescale_gp(gp, o$scale_down)
 
 	coords = sf::st_coordinates(shp)
-	
+
 	# in case shp is a multipoint (point.per == "segment"), expand gp:
 	if (ncol(coords) == 3L) {
 		ndt = nrow(dt)
@@ -389,52 +389,52 @@ tmapLeafletText = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page,
 		text = as.character(dt$text)
 		idt = submit_labels(dt$tmapID__, "text", pane, group)
 	}
-	
+
 	opt = leaflet::pathOptions(interactive = TRUE, pane = pane)
-	
-	
-	
+
+
+
 	cex_set = unique(gp$cex)
-	alpha_set = unique(gp$col_alpha) 
+	alpha_set = unique(gp$col_alpha)
 	face_set = unique(gp$fontface)
 	col_set = unique(gp$col)
-	
+
 	bgcol_set = unique(gp$bgcol)
-	
+
 	if (any(bgcol_set != "#00000000")) {
 		message("Variable bgcol and bgcol_alpha not supported by view mode")
 	}
-	
-	
+
+
 	if (length(face_set) != 1) message("Variable fontfaces not supported by view mode")
-	
+
 	vary = (length(cex_set) != 1) || (length(alpha_set) != 1) || (length(face_set) != 1) || (length(col_set) != 1)
-	
+
 	sizeChar <- paste(round(gp$cex * 12), "px", sep="")
-	
+
 	# direction <- ifelse(gpl$text.just == "left", "right",
 	# 			 ifelse(gpl$text.just == "right", "left",
 	# 			 ifelse(gpl$text.just == "top", "bottom",
 	# 			 ifelse(gpl$text.just == "bottom", "top", "center"))))
-	direction = "right"	
-	
+	direction = "right"
+
 	clustering = args$clustering
-	
+
 	if (identical(clustering, FALSE)) {
 		clustering = NULL
 	} else if (identical(clustering, TRUE)) {
-		clustering = leaflet::markerClusterOptions()	
+		clustering = leaflet::markerClusterOptions()
 	}
-	
+
 	# apply xmod and ymod
 	delta = delta_per_lineheight(bbx)
-	
+
 	coords[,1] = coords[,1] + delta * gp$cex * gp$xmod
 	coords[,2] = coords[,2] + delta * gp$cex * gp$ymod
-	
-	
-	
-	
+
+
+
+
 	if (!vary) {
 		lf = lf %>% addLabelOnlyMarkers(lng = coords[, 1], lat = coords[,2],
 										 label=text,
@@ -467,8 +467,8 @@ tmapLeafletText = function(shpTM, dt, gp, bbx, facet_row, facet_col, facet_page,
 		}
 	}
 	assign_lf(lf, facet_row, facet_col, facet_page)
-	
 
-	
-	NULL	
+
+
+	NULL
 }
