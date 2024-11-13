58625eb90cd5777ed55069d2cae41b35ebb21642
mtennekes <mtennekes@gmail.com>
2024-11-13 11:18:14 +0100

    update tmap_grob #905

--- R/tmap_export.R
+++ R/tmap_export.R
@@ -1,12 +1,13 @@
 #' Export tmap to the format of the used graphics mode
-#' 
+#'
 #' * `tmap_grob()` returns a [`grob`][grid::grob()] object (`"plot" mode`)
 #' * `tmap_leaflet()` a [`leaflet`][leaflet::leaflet()] object (`"view"` mode).
-#' 
+#'
 #' @param x a tmap object.
+#' @param asp,scale the desired aspect ratio and scale of the map. Only applicable for `"plot"` mode.
 #' @param show show the map?
 #' @inheritDotParams print.tmap
-#' @return 
+#' @return
 #' * `tmap_grob()` returns a [`grob`][grid::grob()] object (`"plot"` mode)
 #' * `tmap_leaflet()` a [`leaflet`][leaflet::leaflet()] object (`"view"` mode).
 #'   In case small multiples are shown, a list is returned.
@@ -32,6 +33,8 @@ tmap_leaflet = function(x,
 #' @rdname tmap_leaflet
 #' @export
 tmap_grob = function(x,
+					 asp = NA,
+					 scale = 1,
 					 show = FALSE,
 					 ...) {
 	current_mode = getOption("tmap.mode")
@@ -39,5 +42,5 @@ tmap_grob = function(x,
 		options(tmap.mode = current_mode)
 	})
 	options(tmap.mode = "plot")
-	print.tmap(x, show = show, ...)
+	print.tmap(x + tm_options(asp = asp, scale = scale), show = show, ...)
 }
