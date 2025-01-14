48ac56323cd623b3c94a6daa296e5cf0fb032bef
mtennekes <mtennekes@gmail.com>
2025-01-14 22:28:46 +0100

    ...

--- examples/tm_logo.R
+++ examples/tm_logo.R
@@ -2,8 +2,8 @@ data(World)
 
 tm_shape(World) +
 	tm_polygons("HPI", fill.scale = tm_scale_intervals(values = "RdYlGn")) +
-	tm_logo(c("https://www.r-project.org/logo/Rlogo.png", 
-			  system.file("img/tmap.png", package="tmap"))) +
-	tm_logo("http://blog.kulikulifoods.com/wp-content/uploads/2014/10/logo.png", 
+	tm_logo(c("https://www.r-project.org/logo/Rlogo.png",
+			  system.file("help", "figures", "logo.png", package = "tmap"))) +
+	tm_logo("http://blog.kulikulifoods.com/wp-content/uploads/2014/10/logo.png",
 			height=5, position = c("left", "top")) +
 	tm_format("World")
