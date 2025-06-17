8ac92bcab70acbde725cfcc5299c202ee24747bc
mtennekes <mtennekes@gmail.com>
2025-06-17 14:32:37 +0200

    fixed #1140

--- R/step4_plot.R
+++ R/step4_plot.R
@@ -868,7 +868,7 @@ step4_plot = function(tm, vp, return.asp, show, in.shiny, knit, args) {
 		bbox_nb = d$bbox[1]
 		attr(bbox_nb, "borrow") = list(col = d$col[1], row = d$row[1])
 		legs_out[, bbox:=list(bbox_nb)]
-		legs_out[, units:=d$units[1]]
+		legs_out[, units:=list(d$units[1])]
 
 
 		cdt = data.table::rbindlist(c(list(legs_out), legs_in))
